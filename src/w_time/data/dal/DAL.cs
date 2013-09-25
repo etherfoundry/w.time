ecd6dd903adc426d7f0db929d1397d99705c9583
etherfoundry <etherfoundry@void-media.net>
2013-09-25 17:32:29 -0400

    Adding DAL and Migration classes

---
+++ src/w_time/data/dal/DAL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace w_time.data.dal
+{
+    class DAL
+    {
+    }
+}
---
+++ src/w_time/data/dal/Migration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace w_time.data.dal
+{
+    class Migration
+    {
+        
+    }
+}
