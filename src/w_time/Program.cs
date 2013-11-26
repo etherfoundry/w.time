9f447c1ccb38ae687c63ce25796c7ad17713413c
etherfoundry <etherfoundry@void-media.net>
2013-11-26 01:09:15 -0500

    Stubbed migration checking and running in Application start

--- src/w_time/Program.cs
+++ src/w_time/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 using w_time.Systems.ApplicationSetup;
+using w_time.data;
 
 namespace w_time
 {
@@ -17,8 +18,13 @@ namespace w_time
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
+            // Initialize database environment if its not present
             DataEnvironmentSetup DataSetup = new DataEnvironmentSetup();
             DataSetup.Initialize();
+
+            // Check and run migrations if necessary
+            Migration migration = new Migration();
+            migration.CheckMigrationLevel();
 
             Application.Run(new Form1());
         }
