e8c13363908bb6b7e8689597094dc678f11a0edf
etherfoundry <etherfoundry@void-media.net>
2013-11-26 01:10:36 -0500

    Migration system initialization

--- src/w_time/data/Migration.cs
+++ src/w_time/data/Migration.cs
@@ -7,6 +7,32 @@ namespace w_time.data
 {
     class Migration
     {
-        
+        const string MIGRATION_TABLE = "Migration";
+        /// <summary>
+        /// Checks the current
+        /// </summary>
+        /// <returns></returns>
+        public int CheckMigrationLevel()
+        {
+            CreateMigrationTableIfNotExists();
+            return 0;
+        }
+
+        private bool CheckMigrationTableExists()
+        {
+            DAL dal = new DAL();
+            return dal.CheckTableExists(MIGRATION_TABLE);
+        }
+
+        private void CreateMigrationTableIfNotExists()
+        {
+            DAL dal = new DAL();
+            if (!CheckMigrationTableExists())
+            {
+                // We'll want a better way to do this, but its the only table that cannot be created with migrations
+                dal.ExecuteRawQuery("CREATE TABLE [Migration] ([VersionNumber] int NOT NULL, [Name] nvarchar(300) NOT NULL, [ApplyTime] datetime NOT NULL)");
+                dal.ExecuteRawQuery("ALTER TABLE [Migration] ADD CONSTRAINT [PK_Migration] PRIMARY KEY ([VersionNumber]);");
+            }
+        }
     }
 }
