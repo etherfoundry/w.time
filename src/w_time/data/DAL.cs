49a4ed0b6e6814659ee795fbb2fd463a7c6ba42c
etherfoundry <etherfoundry@void-media.net>
2013-11-26 01:08:51 -0500

    Fleshed out DAL a bit more -- some of this will be reconsidered

--- src/w_time/data/DAL.cs
+++ src/w_time/data/DAL.cs
@@ -2,10 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.SqlServerCe;
 
 namespace w_time.data
 {
     class DAL
     {
+        public bool CheckTableExists(string tableName)
+        {
+            int resultCount = 0;
+            using(SqlCeCommand cmd = this.GetConnection().CreateCommand())
+            {
+                cmd.CommandText = "SELECT COUNT(*) AS TblCount FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = @tablename";
+                cmd.Parameters.AddWithValue("@tablename", tableName);
+                resultCount = (int)cmd.ExecuteScalar();
+            }
+            return resultCount > 0;
+        }
+
+        public int ExecuteRawQuery(string query)
+        {
+            int resultCount = 0;
+            using (SqlCeCommand cmd = this.GetConnection().CreateCommand())
+            {
+                cmd.CommandText = query;
+                resultCount = cmd.ExecuteNonQuery();
+            }
+            return resultCount;
+        }
+
+        private SqlCeConnection GetConnection()
+        {
+            SQLDB.Instance.OpenConnection();
+            return SQLDB.Instance.connection;
+        }
     }
 }
