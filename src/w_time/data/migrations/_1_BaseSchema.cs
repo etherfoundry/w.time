f0aae11c0bc403a43603f2e5a37a95d6bc70a69a
etherfoundry <etherfoundry@void-media.net>
2014-01-24 00:06:21 -0500

    Implemented base schema as migration

--- src/w_time/data/migrations/_1_BaseSchema.cs
+++ src/w_time/data/migrations/_1_BaseSchema.cs
@@ -10,26 +10,57 @@ namespace w_time.data.migrations
     {
         public override bool up()
         {
-            Table EventTable = new Table("Event");
+            Table EventTypeTable = new Table("EventType");
+            EventTypeTable.Columns.Add(new PrimaryKeyIntColumn());
+            EventTypeTable.Columns.Add(new NVarCharColumn(45) { Name = "Name" });
 
-            EventTable.Columns.Add(new BigIntColumn() {
-                    Name = "ID",
-                    IsPrimaryKey = true,
-                    IsIdentity = true,
-                    IdentityIncrement = 1,
-                    IdentitySeed = 1,
-                });
+            Table ProcessTable = new Table("Process");
+            ProcessTable.Columns.Add(new PrimaryKeyIntColumn());
+            ProcessTable.Columns.Add(new NVarCharColumn(255) { Name = "Name" });
+
+            Table CategoryTable = new Table("Category");
+            CategoryTable.Columns.Add(new PrimaryKeyIntColumn());
+            CategoryTable.Columns.Add(new IntegerColumn() { Name = "ParentCategory_FK",
+                ReferencesColumn = "ID", ReferencesTable = "Category"});
+            CategoryTable.Columns.Add(new NVarCharColumn(100) { Name = "Name" });
+
+            Table EventTable = new Table("Event");
+            EventTable.Columns.Add(new PrimaryKeyIntColumn(Column.DataType.bigint));
 
             EventTable.Columns.Add(new DateTimeColumn() { Name = "Time" });
-            EventTable.Columns.Add(new NumericColumn(10,3) { Name = "Duration" });
-            EventTable.Columns.Add(new IntegerColumn() { Name = "EventType_FK" });
-            EventTable.Columns.Add(new IntegerColumn() { Name = "Process_FK", IsNullable = true });
-            EventTable.Columns.Add(new IntegerColumn() { Name = "Category_FK", IsNullable = true });
+            EventTable.Columns.Add(new NumericColumn(10, 3) { Name = "Duration" });
+            EventTable.Columns.Add(new IntegerColumn()
+            {
+                Name = "EventType_FK",
+                ReferencesTable = "EventType",
+                ReferencesColumn = "ID",
+            });
+
+            EventTable.Columns.Add(new IntegerColumn()
+            {
+                Name = "Process_FK",
+                IsNullable = true,
+                ReferencesTable = "Process",
+                ReferencesColumn = "ID",
+            });
+
+            EventTable.Columns.Add(new IntegerColumn()
+            {
+                Name = "Category_FK",
+                IsNullable = true,
+                ReferencesTable = "Category",
+                ReferencesColumn = "ID",
+            });
+
             EventTable.Columns.Add(new NVarCharColumn(300) { Name = "WindowTitle", IsNullable = true });
             EventTable.Columns.Add(new NVarCharColumn(200) { Name = "Notes", IsNullable = true });
+
+            this.CreateTable(EventTypeTable);
+            this.CreateTable(ProcessTable);
+            this.CreateTable(CategoryTable);
+
+            // References Process and Category
             this.CreateTable(EventTable);
-
-
             return false;
         }
 
