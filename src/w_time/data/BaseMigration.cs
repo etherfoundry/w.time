59fc4db48912a98d8c32b7744f5b49b7f7d8a6e2
etherfoundry <etherfoundry@void-media.net>
2014-01-23 23:44:32 -0500

    Base migration now actually executes create table commands through DAL

--- src/w_time/data/BaseMigration.cs
+++ src/w_time/data/BaseMigration.cs
@@ -11,10 +11,11 @@ namespace w_time.data
         public abstract bool up();
         public abstract bool down();
 
-
         protected void CreateTable(Table table)
         {
-            Console.WriteLine(table.GetCreateTableSQL());
+            System.Diagnostics.Debug.WriteLine(table.GetCreateTableSQL());
+            DAL dal = new DAL();
+            dal.ExecuteRawQuery(table.GetCreateTableSQL());
         }
     }
 }
